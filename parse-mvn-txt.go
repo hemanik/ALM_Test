257a114bb6a747a1e0e3fe38b2d41b2995936ae9
Hemani <hemani@dhcp35-120.lab.eng.blr.redhat.com>
2016-08-29 14:41:57 +0530

    Updated Parsing for Text Format

--- parse-mvn-txt.go
+++ parse-mvn-txt.go
@@ -5,7 +5,7 @@ import (
      "regexp"
      "fmt"
      "os"
-     "io/ioutil"
+     "strings"
 )
 
 func main() {
@@ -19,32 +19,33 @@ func main() {
           Time string 
      }
 
-     v := TestResult{}
+     //v := TestResult{}
 
      f, _ := os.Open("test.txt")
      scanner := bufio.NewScanner(f)
      
-     b, _ := ioutil.ReadAll("test.txt")
-
-     re := regexp.MustCompile("Test Set: [^,\s]*")     
-
-     fmt.Print("%q\n", re.FindString(b)) 
-     //scanner.Split(bufio.ScanWords)
- 
      for scanner.Scan() {
-         line := scanner.Text()
-         pattern := `\w+(\s\w+)?: [^,\s]*` 
-         re := regexp.MustCompile(pattern)
+          line := scanner.Text()
+         
+          pattern := `\w+(\s\w+)?: [^,\s]*`
+          tmp:= regexp.MustCompile(pattern)
+          str := tmp.FindAllString(line, -1)
        
+          values := []string{"Test set", "Tests run" , "Failures", "Errors", "Skipped", "Time elapsed"}
 
-           
-       // fmt.Println(line)
-       // re := regexp.MustCompile(`[A-Za-z]+(\s[A-Za-z]+)?:`)
-       // re := regexp.MustCompile(`\w+(\s\w+)?: [^,\s]*`)
-         fmt.Printf("%q\n", re.FindAllString(line, -1))
+          for _, i := range str {
+                          
+               x := strings.Split(i, ":")
+               
+               for  _, b := range values { 
+               
+                    if x[0] == b {
+                        //Add to Struct
+                        fmt.Print("\n", x[1])
+                        break 
+                    }
+               }
+          }    
      }
-
-     //fmt.Printf("%q\n", strings.Split("a,b,c", ","))
- 
 }
 	
